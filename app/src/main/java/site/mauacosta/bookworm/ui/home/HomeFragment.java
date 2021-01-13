aff71133d1d624a20f0026cb47115d9f71881c50
AlejandraNissan <31284199+AlejandraNissan@users.noreply.github.com>
2021-01-13 13:08:05 -0600

    Se agregó shared prefs al boton de settings para saludo

--- app/src/main/java/site/mauacosta/bookworm/ui/home/HomeFragment.java
+++ app/src/main/java/site/mauacosta/bookworm/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package site.mauacosta.bookworm.ui.home;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,18 +15,32 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import org.w3c.dom.Text;
+
 import site.mauacosta.bookworm.MainActivity;
 import site.mauacosta.bookworm.R;
 
 public class HomeFragment extends Fragment {
 
+    TextView welcomeText;
+
     private HomeViewModel homeViewModel;
+    public SharedPreferences prefs;
+    private static final String ARCHIVO_PREFS = "misPrefs";
+    private static final String KEY_NAME = "nombre";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        prefs = getActivity().getSharedPreferences(ARCHIVO_PREFS, Context.MODE_PRIVATE);
+
+        welcomeText = root.findViewById(R.id.tvHomeTitle);
+
+        welcomeText.setText("WELCOME " + prefs.getString(KEY_NAME, "WELCOME"));
+
         return root;
     }
 
--- app/src/main/java/site/mauacosta/bookworm/ui/settings/SettingsFragment.java
+++ app/src/main/java/site/mauacosta/bookworm/ui/settings/SettingsFragment.java
@@ -1,12 +1,16 @@
 package site.mauacosta.bookworm.ui.settings;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,15 +24,37 @@ import site.mauacosta.bookworm.R;
 
 public class SettingsFragment extends Fragment {
 
+    public SharedPreferences prefs;
+    private static final String ARCHIVO_PREFS = "misPrefs";
+    private static final String KEY_NAME = "nombre";
     private SettingsViewModel settingsViewModel;
     EditText nombre;
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        settingsViewModel =
-                new ViewModelProvider(this).get(SettingsViewModel.class);
+    Button boton;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        settingsViewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        return root;
+
+
+        nombre = root.findViewById(R.id.etChangeName);
+        boton = root.findViewById(R.id.btnChangeName);
+
+        prefs = getActivity().getSharedPreferences(ARCHIVO_PREFS, Context.MODE_PRIVATE);
+
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString(KEY_NAME, nombre.getText().toString());
+                editor.commit();
+            }
+        });
+
+            return root;
+
     }
+
 
     public void goToChooseBook(View v){
         ((MainActivity) getActivity()).goToAddBook(v);
